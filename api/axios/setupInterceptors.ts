2b37b87a2687ae9e70695c6ecb57a1e0e050b43e
thanh0092 <thanh.bui@tomosia.com>
2024-06-07 17:07:59 +0700

    chore: axiosConfigUrl

--- api/axios/setupInterceptors.ts
+++ api/axios/setupInterceptors.ts
@@ -5,12 +5,14 @@ import { onResponse } from "./handleResponse";
 import { API_BASE_URL } from "@/utils/config";
 
 export const setupInterceptors = (instance: AxiosInstance): AxiosInstance => {
-    instance.defaults.baseURL = API_BASE_URL;
+  instance.defaults.baseURL = API_BASE_URL + "/rest/v1";
 
     instance.defaults.paramsSerializer = (params) => {
       return Object.keys(params)
-        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-        .join('&');
+      .map(
+        (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+      )
+      .join("&");
     };
     instance.interceptors.request.use(onRequest, onErrorResponse);
     instance.interceptors.response.use(onResponse, onErrorResponse);
